bd28a22e9f8de76b86075d966856d02c1e2ea8ef
hlamy <hugues.lamy@gmail.com>
2020-11-12 17:42:48 +0100

    A lot of work done. Added and updated fonctions in "pichandler" Tested fonctionnality Added start of JSON handling

--- hlamy_main.py
+++ hlamy_main.py
@@ -1,15 +1,93 @@
-import json
 import pprint
-from flask import Flask
+from random import randint
+from flask import Flask, send_from_directory, request, abort, send_file
+import os
+from PIL import Image as img
+# utilisation de la librairie 'Path' pour assurer une bonne gestion des chemins de fichiers
+from pathlib import Path
+# importation des fonctions du service de gestion des images
+import pichandler
 
 app = Flask(__name__)
 texte = pprint.pformat(__name__) + '\n'
 
+# lignes de configuration des dossiers de stockage des images, thumbnails et fichiers temporaires
+app.config['UPLOAD_FOLDER'] = 'f:/GitHub/SIO_Python_HLAMY/Temp'
+temporary_files_folder = 'f:/GitHub/SIO_Python_HLAMY/Temp'
+pictures_folder = Path('f:/GitHub/SIO_Python_HLAMY/pictures')
+thumbnails_folder = Path('f/GitHub/SIO_Python_HLAMY/thumbnails')
+app.config["CLIENT_IMAGES"] = pictures_folder
+metadata_folder = Path('f:/GitHub/SIO_Python_HLAMY/metadata')
+
 #page racine de l'API
 @app.route('/')
 def mainpage():
-    page = "Test 2"
-    return page
+    picturename = 'pictures/test.jpg'
 
-# lancement de l'application :
-app.run()
+    return pichandler.extractMetadata(picturename)
+
+# méthode de dépot de l'image sur le serveur (via POST)
+@app.route('/images', methods = ['POST'])
+def uploadpic():
+    
+    # obtention d'un identifiant d'image
+    try :
+        pictureID = pichandler.definePictureID(pictures_folder)
+    except:
+        return 'Error : could not provide proper ID'
+    # try to load the provided file. If not OK, return error
+    
+    try:
+        picture = request.files['file']
+    except:
+        return 'Error : upload problem'
+    
+    # sauvegarde l'image dans un dossier temporaire et extraction des metadata
+    try:
+        picture.save(temporary_files_folder / Path(pictureID))
+        #pichandler.saveMetadataAsJSON(temporary_files_folder / Path(pictureID), metadata_folder)
+    except:
+        return 'Error : metadata extraction problem'
+
+    # try to open provided file as a picture. If OK, save it, if not, return error
+    try:
+        
+        with img.open(picture) as new_pic:
+            new_pic.save(str(pictures_folder / Path(pictureID + '.' + new_pic.format)))
+        
+    except:
+        return 'Error : file is not a picture'
+    
+    try:
+        if pichandler.saveMetadataAsJSON(pictureID, pichandler.extractMetadata(str(temporary_files_folder / Path(pictureID))), metadata_folder):
+        
+            return pictureID
+        else:
+            return 'no metadata'
+    except:
+        abort(500)
+
+
+
+# méthode d'envoi vers le client des métadata de l'image demandée
+@app.route('/images/metadata/<pictureID>', methods = ['GET'])
+def metadataaccess(pictureID):
+    try:
+        return send_file(str(pictures_folder / Path(pictureID + '.json')), as_attachment=True)
+    # si fichier non trouvé : erreur 404, car cas similaire à une page non trouvée
+    except FileNotFoundError:
+        abort(404)
+
+
+# méthode d'envoi de l'image demandée vers le client de l'API (via GET) - façon téléchargement
+@app.route('/images/<picturename>', methods = ['GET'])
+def pictureaccess(picturename):
+    try:
+        return send_file(str(pictures_folder / Path(picturename)), as_attachment=True)
+    # si fichier non trouvé : erreur 404, car cas similaire à une page non trouvée
+    except FileNotFoundError:
+        abort(404)
+
+
+
+
---
+++ launcher.py
@@ -0,0 +1,5 @@
+# importation de l'API
+import hlamy_main
+
+# lancement du serveur flask et de l'application :
+hlamy_main.app.run()
